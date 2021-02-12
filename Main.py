e5bc58d55537caf31ce0ade1ffc52fcfe9386168
BoredAllWays <saipranav64@gmail.com>
2021-02-12 16:18:13 -0500

    saved main.py

--- Main.py
+++ Main.py
@@ -16,3 +16,21 @@ if __name__ == "__main__":
     TOKEN = data['BOT TOKEN']
 
     bot.run(TOKEN)
+import discord
+from discord.ext import commands
+import os
+import json
+bot = commands.Bot(command_prefix="[]")
+
+if __name__ == "__main__":
+    for i in os.listdir("cogs"):
+        if i.endswith(".py"):
+            bot.load_extension(f"cogs.{i[:-3]}")
+            print(str(i) + ' is ready')
+
+    with open('./SecretStuff.json') as f:
+        data = json.load(f)
+
+    TOKEN = data['BOT TOKEN']
+
+    bot.run(TOKEN)
